329d40ad4c8eb9e5e6615ff3555f32a1bca53899
Sebastian Puszies <s.puszies@fielmann.com>
2023-11-02 23:47:25 +0100

    adding icons to button

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -12,10 +12,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'frontend'`, () => {
+  it(`should have as title 'StoneSissorsPaper'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('frontend');
+    expect(app.title).toEqual('StoneSissorsPaper');
   });
 
   it('should render title', () => {
--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatIconRegistry } from "@angular/material/icon";
+import { DomSanitizer } from "@angular/platform-browser";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(
+    private matIconRegistry: MatIconRegistry,
+    private domSanitizer: DomSanitizer
+    ) {
+    this.matIconRegistry.addSvgIcon(
+      `stone`,
+      this.domSanitizer.bypassSecurityTrustResourceUrl(`../assets/svg/stone.svg`)
+    );
+    this.matIconRegistry.addSvgIcon(
+      `sissors`,
+      this.domSanitizer.bypassSecurityTrustResourceUrl(`../assets/svg/scissors.svg`)
+    );
+    this.matIconRegistry.addSvgIcon(
+      `paper`,
+      this.domSanitizer.bypassSecurityTrustResourceUrl(`../assets/svg/paper.svg`)
+    );
+  }
   title = 'StoneSissorsPaper';
 }
